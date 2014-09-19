8407f395525807524ed795f602faba21caed3e53
David <davidborodin81@gmail.com>
2014-09-19 20:22:55 +0400

    products page

--- protected/modules/Pages/controllers/PageController.php
+++ protected/modules/Pages/controllers/PageController.php
@@ -1,49 +1,107 @@
 <?php
 
-class PageController extends Controller
-{
-	public function actionIndex()
-	{
-            $page = new Page();
-            $RetailerCategory  = new RetailerCategory();
-            
-            $page_category = new PageCategory();
-            
-            $Pages = $page->findAll();
-            $page_arr = array();
-            foreach ($Pages as $page){
-                $page_arr[$page->id] = $page->title;
-            }
-            
-            $categories = RetailerCategory::model()->findAll();
-            $category_arr = array();
-            foreach ($categories as $cetgory){
-                $category_arr[$cetgory->id] = $cetgory->name;
-            }
-            
-            
-            if(isset($_POST['PageCategory'])) {
+class PageController extends Controller {
 
-                $post_page = Yii::app()->request->getPost('PageCategory');
-                $post_page_id = Page::model()->findByPk($post_page['page_id']);
-                $post_category_id = Page::model()->findByPk($post_page['category_id']);
-                 
-                if($post_page_id != Null && $post_category_id != Null){
-                    $issetPage= PageCategory::model()->findByPk($post_page['page_id']);
-                        if($issetPage == Null){
-                            $page_category->setAttributes($post_page);
-                            if($page_category->validate()){
-                                $page_category->save();
-                            } 
-                        }else{
-                            $issetPage->setAttributes($post_page);
-                            if($issetPage->validate()){
-                                $issetPage->save();
-                            }                             
-                        }                                                                     
+    public function actionSaveProduct() {
+//        if(!empty($_POST)  && isset($_POST['data'])) {
+//            $new_product = new PageProducts();
+//            $prod_info = serialize($_POST['data']);
+//        }
+//        var_dump($_POST['data']);die;
+        
+    }
+
+
+    public function actionIndex() {
+        
+        $page = new Page();
+        $Product_pages = new PageProducts();
+        $RetailerCategory = new RetailerCategory();
+
+        $page_category = new PageCategory();
+
+        $Pages = $page->findAll();
+        $page_arr = array();
+        foreach ($Pages as $page) {
+            $page_arr[$page->id] = $page->title;
+        }
+
+        $categories = RetailerCategory::model()->findAll();
+        $category_arr = array();
+        foreach ($categories as $cetgory) {
+            $category_arr[$cetgory->id] = $cetgory->name;
+        }
+
+        $render_arr = array('pages' => $page_category, 'page_arr' => $page_arr, 'category_arr' => $category_arr, 'model_prod' => $Product_pages);        
+
+        if (isset($_POST['PageCategory'])) {
+
+
+            $post_page = Yii::app()->request->getPost('PageCategory');
+//                $page_product = PageProducts::model()->find("page_id = ".$post_page['page_id']);
+            $post_page_id = Page::model()->findByPk($post_page['page_id']);
+            $post_category_id = $page_category->findByPk($post_page['category_id']);
+
+            if ($post_page_id != Null && $post_category_id != Null) {
+                //$issetPage = PageCategory::model()->findByPk($post_page['page_id']);
+                $issetPage = PageCategory::model()->findByAttributes(array(
+                    'page_id' => $post_page['page_id']
+                ));
+
+                if ($issetPage == Null) {
+                    $page_category->setAttributes($post_page);
+                    if ($page_category->validate()) {
+                        $page_category->save();
+                    }
+                } else {
+
+                    $issetPage->setAttributes($post_page);
+                    if ($issetPage->validate()) {
+
+                        $issetPage->save();
+                    }
                 }
+
+                $query_params = PageCategory::model()->find("page_id = " . $post_page['page_id']);
+
+                $categories = RetailerCategory::model()->findByPk($query_params['category_id']);
+                $url = 'http://productsearch.linksynergy.com/productsearch?token=004fdfcbd783c723a20436a65dab14dcd57c6094a9db8cb400bb866fd778e1a9&keyword=' . $categories['name'] . '&cat=' . $categories['name'] . '&MaxResults=' . $query_params['count'] . '&pagenumber=1&mid=2557&sort=retailprice&sorttype=asc&sort=productname&sorttype=asc';
+                $products = $this->curl_get_contents($url);
+                $render_arr = array('pages' => $page_category, 'page_arr' => $page_arr, 'category_arr' => $category_arr, 'productCount' => $query_params['count'], 'products' => $products, 'model_prod' => $Product_pages);
+
+//                        }
             }
-            
-            $this->render('index',array('pages'=>$page_category,'page_arr'=>$page_arr,'category_arr'=>$category_arr));
-	}
+        }
+
+        $this->render('index', $render_arr);
+    }
+
+    function curl_get_contents($url) {
+
+
+        $curl = curl_init($url);
+
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(header('Content-Type: application/Json')));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+
+
+        $data = curl_exec($curl);
+
+        curl_close($curl);
+
+        $data = simplexml_load_string($data);
+
+        return array(
+            'data' => $data
+        );
+    }
+
 }
